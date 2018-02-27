fbbbd9c3d4caa85821844215eb0595a6176300b9
tamuka120 <tamuka120@users.noreply.github.com>
2018-02-27 14:52:12 +0000

    New homepage interface
    Signed-off-by: tamuka120 <tamuka120@users.noreply.github.com>

--- app/src/main/java/comtamuka120/httpsgithub/final_project/MainActivity.java
+++ app/src/main/java/comtamuka120/httpsgithub/final_project/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,26 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        ImageButton imgbtn = (ImageButton) findViewById(R.id.imgbtn);
-//        imgbtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent int_1 = new Intent(MainActivity.this, page_2.class);
-//                startActivity(int_1);
-//            }
-//        });
-
-        //get the spinner from the xml.
-        Spinner dropdown = findViewById(R.id.spinner1);
-         //create a list of items for the spinner.
-        String[] items = new String[]{"Home", "Settings", "Games"};
-        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
-        //There are multiple variations of this, but this is the basic variant.
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        //set the spinners adapter to the previously created one.
-        dropdown.setAdapter(adapter);
 
 
+        }
 
-    }
 }
--- app/src/main/java/comtamuka120/httpsgithub/final_project/page_3.java
+++ app/src/main/java/comtamuka120/httpsgithub/final_project/page_3.java
@@ -3,17 +3,11 @@ package comtamuka120.httpsgithub.final_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class page_2 extends AppCompatActivity {
+public class page_3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page_2);
-
-
-
-
-
-
+        setContentView(R.layout.activity_page_3);
     }
 }
