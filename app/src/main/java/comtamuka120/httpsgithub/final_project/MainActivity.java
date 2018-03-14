bdabadf28bb8fc2dbac0abeb99bf0f9de8593c5b
tamuka120 <tamuka120@users.noreply.github.com>
2018-03-14 00:30:22 +0000

    1. Implement Firebase 2. Created Database for Accounts 3. Implement Login and Registration Functionality
    Signed-off-by: tamuka120 <tamuka120@users.noreply.github.com>

--- app/src/main/java/comtamuka120/httpsgithub/final_project/MainActivity.java
+++ app/src/main/java/comtamuka120/httpsgithub/final_project/MainActivity.java
@@ -1,32 +1,81 @@
 package comtamuka120.httpsgithub.final_project;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtEmail;
+    private EditText txtPass;
+    private FirebaseAuth firebaseAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button reg_btn =(Button) findViewById(R.id.reg_button);
-
-        reg_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, registration.class));
-            }
-        });
-
+        txtEmail = findViewById(R.id.login_user_ediText);
+        txtPass = findViewById(R.id.pass_login_editText);
+        firebaseAuth = FirebaseAuth.getInstance();
         }
 
+
+    public void reg_button_loginClick(View v) {
+        Intent  intent = new Intent(MainActivity.this, registration.class);
+        startActivity(intent);
+    }
+
+    public void login_buttonClick(View v) {
+
+        if(txtEmail.getText().toString().equals("") || txtPass.getText().toString().equals("")){
+            Toast.makeText(MainActivity.this, "Username or Password is Blank", Toast.LENGTH_LONG).show();
+
+
+
+        } else {
+
+
+            final ProgressDialog progressDialog = ProgressDialog.show(MainActivity.this, "Please Wait...", "Processing", true);
+            (firebaseAuth.signInWithEmailAndPassword(txtEmail.getText().toString(), txtPass.getText().toString()))
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressDialog.dismiss();
+                            if (task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Welcome!", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(MainActivity.this, blank_test.class);
+                                intent.putExtra("Email", firebaseAuth.getCurrentUser().getEmail());
+                                startActivity(intent);
+                            } else {
+                                Log.e("ERROR", task.getException().toString());
+                                Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
+        }
+
+
+    }
 }
--- app/src/main/java/comtamuka120/httpsgithub/final_project/registration.java
+++ app/src/main/java/comtamuka120/httpsgithub/final_project/registration.java
@@ -1,13 +1,82 @@
 package comtamuka120.httpsgithub.final_project;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class registration extends AppCompatActivity {
+
+//    private EditText txtDob;
+//    private EditText txtUser;
+    private EditText txtEmail;
+    private EditText txtPass;
+
+    private FirebaseAuth firebaseAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+
+        txtEmail = findViewById(R.id.email_reg_editText);
+        txtPass = findViewById(R.id.pass_reg_editText);
+//        txtDob = findViewById(R.id.dob_reg_editText);
+//        txtUser = findViewById(R.id.user_reg_editText);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        }
+
+    public void reg_btnClick(View v) {
+
+        if(txtEmail.getText().toString().equals("") || txtPass.getText().toString().equals("")){
+            Toast.makeText(registration.this, "Blank field", Toast.LENGTH_LONG).show();
+
+
+
+        } else {
+
+
+            final ProgressDialog progressDialog = ProgressDialog.show(registration.this, "Please Wait...", "Processing", true);
+            (firebaseAuth.createUserWithEmailAndPassword(txtEmail.getText().toString(), txtPass.getText().toString()))
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressDialog.dismiss();
+
+                            if (task.isSuccessful()) {
+                                Toast.makeText(registration.this, "Registration Successful!", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(registration.this, MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Log.e("ERROR", task.getException().toString());
+                                Toast.makeText(registration.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+
+                            }
+
+
+                        }
+                    });
+
+        }
+
     }
 }
